c70fefc0d37f99e39d61a4c2126d4d5a2c75b9af
harshal <harshalh.cse25@sbjit.edu.in>
2026-04-30 00:49:03 +0530

    scrolling effect top to bottom

--- script.js
+++ script.js
@@ -51,4 +51,14 @@ window.onload=function(){
 setTimeout(()=>{
 alert("Welcome to Computer Science Department Website");
 },800);
-};
+};let topBtn=document.createElement("button");
+
+topBtn.innerHTML="↑";
+topBtn.style.position="fixed";
+topBtn.style.bottom="20px";
+topBtn.style.right="20px";
+topBtn.style.padding="12px 16px";
+topBtn.style.border="none";
+topBtn.style.borderRadius="50%";
+topBtn.style.cursor="pointer";
+topBtn.style.display="none";
