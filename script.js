b8ba81dd941dcf03cfaae1418ff560b9d97f63d9
harshal <harshalh.cse25@sbjit.edu.in>
2026-04-30 00:46:55 +0530

    card hover effect and transition

--- script.js
+++ script.js
@@ -33,3 +33,17 @@ link.style.color="yellow";
 });
 
 });
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+card.addEventListener("mouseenter",()=>{
+card.style.transform="scale(1.05)";
+card.style.transition="0.3s";
+});
+
+card.addEventListener("mouseleave",()=>{
+card.style.transform="scale(1)";
+});
+
+});
