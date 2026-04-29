eb194cb3ae99243a38ca1ae093711734345fd53d
harshal <harshalh.cse25@sbjit.edu.in>
2026-04-30 00:37:22 +0530

    add smoothly transition

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.querySelectorAll('.navbar a').forEach(link=>{
+    link.addEventListener('click',function(e){
+e.preventDefault();
+
+let target=document.querySelector(this.getAttribute('href'));
+
+target.scrollIntoView({
+behavior:'smooth'
+});
+});
+});
